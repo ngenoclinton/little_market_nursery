b1bb1e3c6e0c43edba9692e7ab27e46831da48e9
ngenoclinton <clintonkipkoechngeno@gmail.com.com>
2025-04-24 23:29:32 +0300

    Nursery-logo f

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+    return (
+      <>
+        <meta charSet="utf-8" />
+        <title>Little Market Nursery</title>
+        <link rel="icon" href="/Nursery-logo.png" />
+        <link rel="icon" href="/Nursery-logo.svg" type="image/svg+xml" />
+      </>
+    )
+  }
--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const quicksand = Quicksand({
 })
 
 export const metadata: Metadata = {
-  title: "Kid's Promising Tomorrow - Nurturing Young Minds",
+  title: "Little Market Nursery- Nurturing Young Minds",
   description: "A nurturing environment for children to learn, grow, and thrive.",
 }
 
